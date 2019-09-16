c3a667f34109a61a123ea673aab317fa2d869f12
Manthan Jaiswal <manthanjaiswal2@gmail.com>
2019-09-16 09:27:10 +0530

    Fisher yates suffle

---
+++ 10-fisher-yates.js
@@ -0,0 +1,21 @@
+//Fisher-Yates shuffle
+
+
+function shuffle(array) {
+  var copy = [], n = array.length, i;
+
+  // While there remain elements to shuffle…
+  while (n) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * n--);
+
+    // And move it to the new array.
+    copy.push(array.splice(i, 1)[0]);
+  }
+
+  return copy;
+}
+
+console.log(shuffle([1,2,3,8,5,4,6]));
+
