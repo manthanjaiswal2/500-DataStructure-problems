f819b38d5bdc60e16d286a587e94ac7ec05f98d6
Manthan Jaiswal <manthanjaiswal2@gmail.com>
2019-09-16 23:18:14 +0530

    Selection Sorting

---
+++ 31-selection-sort.js
@@ -0,0 +1,22 @@
+//Selection sort
+
+
+let selectionSort = (arr) => {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        if (min !== i) {
+            let tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+    }
+    return arr;
+}
+
+console.log(selectionSort([5,8,45,23,6,20,52]));
