e7f5e93819c73efae32e8c1a7c0b7a9730e2ffb5
Manthan Jaiswal <manthanjaiswal2@gmail.com>
2019-09-16 09:25:00 +0530

    Merge arrays

---
+++ 7-merge-arrays.js
@@ -0,0 +1,31 @@
+// Merge two sorted arrays
+
+function mergeTwo(arr1, arr2) {
+  let merged = [];
+  let index1 = 0;
+  let index2 = 0;
+  let current = 0;
+
+  while (current < (arr1.length + arr2.length)) {
+      let unmerged1 = arr1[index1];
+      let unmerged2 = arr2[index2];
+
+      // if next comes from arr1
+      if (unmerged1 < unmerged2) {
+          merged[current] = unmerged1;
+          index1++;
+
+      // if next comes from arr2
+      } else {
+          merged[current] = unmerged2;
+          index2++;
+      }
+
+      current++;
+  }
+
+  return merged;
+}
+
+console.log(mergeTwo([1,2,3],[4,5,6]));
+
